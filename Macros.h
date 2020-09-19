b562703d166bfd7a474b7186e778697aa2c86f08
alexbarra <alexbarra00@gmail.com>
2020-09-19 21:30:41 +0200

    Update Macros.h
    -Update Macros.h

--- Macros.h
+++ Macros.h
@@ -2,9 +2,11 @@
 #define MACROS_H
 
 //	FPS
+#define FPS_30 30
 #define FPS_60 60
-#define FPS_120 
-#define FPS_144 155
+#define FPS_120 120
+#define FPS_144 144
+#define FPS_244 244
 
 
 #endif // !MACROS_H
