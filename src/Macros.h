132ffb21d68cb1e2a99ce50b6fadaf06ff8707ac
alexbarra <alexbarra00@gmail.com>
2020-09-25 16:50:59 +0200

    Added new resolution
    -Added new resolution

--- src/Macros.h
+++ src/Macros.h
@@ -11,15 +11,18 @@
 
 //	WINDOW SIZES
 #define W_WIDTH_480 640		//	VGA
+#define W_WIDTH_600 800		//	SVGA
 #define W_WIDTH_720 1280	//	HD
 #define W_WIDTH_900 1600	//	HD+
 #define W_WIDTH_1080 1920	//	FHD
 #define W_WIDTH_1440 2560	//	QHD
 
 #define W_HEIGHT_480 480	//	VGA
+#define W_HEIGHT_600 600	//	SVGA
 #define W_HEIGHT_720 720	//	HD
-#define W_HEIGHT_900 900		//	HD+
+#define W_HEIGHT_900 900	//	HD+
 #define W_HEIGHT_1080 1080	//	FHD
 #define W_HEIGHT_1440 1440	//	QHD
+
 
 #endif // !MACROS_H
