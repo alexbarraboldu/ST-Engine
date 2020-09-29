629f31a1058c2e3a331fc43ee77afe2270d25195
alexbarra <alexbarra00@gmail.com>
2020-09-29 12:54:40 +0200

    Added VSYNC GlobalVar enum
    Added VSYNC GlobalVar enum

--- src/Structures.h
+++ src/Structures.h
@@ -8,8 +8,8 @@ typedef enum {
 	FRAME_RATE = 1,
 	QUITE_ENGINE_LOOP = FRAME_RATE << 1,
 	WIDTH_HEIGHT = QUITE_ENGINE_LOOP << 1,
-	D = WIDTH_HEIGHT << 1,
-	E = D << 1,
+	VSYNC = WIDTH_HEIGHT << 1,
+	E = VSYNC << 1,
 
 	NO_PERMISSION = E << 1,
 
