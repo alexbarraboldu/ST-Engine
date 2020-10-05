3d5d10984a058d5aafa47164717307f2f0147a8a
alexbarra <alexbarra00@gmail.com>
2020-10-05 12:17:57 +0200

    Made it functioning and adapted to Button class functioning
    -Changed internal members to match Button class ones
    -Fixed and improved DropdownButtons functioning
    
    // NOTE
    Yeah the name is wrong XD, in the next push I'll changed it.

--- src/buttons/DropdownButton.h
+++ src/buttons/DropdownButton.h
@@ -7,14 +7,23 @@ class DropdownButton : public Button
 {
 public:
 	DropdownButton();
+	DropdownButton(Uint16 W, Uint16 H);
+	DropdownButton(Uint16 W, Uint16 H, SDL_Color _color);
 	~DropdownButton();
 
 	virtual void update();
 	virtual void render();
 
 private:
-	void dropdownButton();
-	void dropdownDraw();
+	void openBoxUpdate();
+	void openBoxDraw();
+
+	SDL_Rect DP_Rect;
+	SDL_Color DP_Color;
+	bool DP_Pressed;
+	bool DP_Used;
+
+	bool isBoxOpen;
 };
 
 #endif // !DROP_DOWN_BUTTON_H
--- src/buttons/DropwdownButtoncpp.cpp
+++ src/buttons/DropwdownButtoncpp.cpp
@@ -3,6 +3,28 @@
 
 DropdownButton::DropdownButton() : Button()
 {
+	DP_Rect = { B_Rect.x + B_Rect.w - 25, B_Rect.y + B_Rect.h - 25, 25, 25};
+	DP_Color = B_Color;
+	DP_Pressed = false;
+	DP_Used = false;
+}
+
+DropdownButton::DropdownButton(Uint16 W, Uint16 H) : Button()
+{
+	DP_Rect = { B_Rect.x + B_Rect.w - W, B_Rect.y + B_Rect.h - H, W, H };
+	DP_Color = B_Color;
+	DP_Pressed = false;
+	DP_Used = false;
+}
+
+DropdownButton::DropdownButton(Uint16 W, Uint16 H, SDL_Color _color) : Button()
+{
+	DP_Rect = { B_Rect.x + B_Rect.w - W, B_Rect.y + B_Rect.h - H, W, H };
+	DP_Color = _color;
+	DP_Pressed = false;
+	DP_Used = false;
+
+	isBoxOpen = false;
 }
 
 DropdownButton::~DropdownButton()
@@ -11,27 +33,36 @@ DropdownButton::~DropdownButton()
 
 void DropdownButton::update()
 {
-	clickToButton();
-	dropdownButton();
+	clickToButton(DP_Rect, DP_Pressed, DP_Used);
+	openBoxUpdate();
 }
 
 void DropdownButton::render()
 {
-	drawBasicButton();
-	dropdownDraw();
+	basicButtonDraw(B_Rect);
+	basicButtonDraw(DP_Rect, DP_Color, DP_Pressed, false);
+	openBoxDraw();
 }
 
-void DropdownButton::dropdownButton()
+void DropdownButton::openBoxUpdate()
 {
-	if (sInput->isKeyPressed(SDL_SCANCODE_C)) {
-		printf("\nDropdown Button Update\n");
+	if (DP_Used && !isBoxOpen)
+	{
+		isBoxOpen = true;
+	}
+	else if (DP_Used && isBoxOpen)
+	{
+		isBoxOpen = false;
 	}
 }
 
-void DropdownButton::dropdownDraw()
+void DropdownButton::openBoxDraw()
 {
-	if (sInput->isKeyPressed(SDL_SCANCODE_B)) {
-		printf("\nDropdown Button Render\n");
+	if (isBoxOpen)
+	{
+		SDL_SetRenderDrawBlendMode(sRenderer->getRenderer(), SDL_BLENDMODE_BLEND);
+
+		SDL_SetRenderDrawColor(sRenderer->getRenderer(), B_Color.r, B_Color.g, B_Color.b, Uint8(B_Color.a / 2.55));
+		SDL_RenderFillRect(sRenderer->getRenderer(), new SDL_Rect{ B_Rect.x, B_Rect.y + B_Rect.h, B_Rect.w, B_Rect.h * 3 });
 	}
 }
-
