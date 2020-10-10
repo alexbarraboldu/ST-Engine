1818158dac88e90beba728d8bbad519e6d9f4ca4
alexbarra <alexbarra00@gmail.com>
2020-10-10 14:58:50 +0200

    Upgraded Text.h
    -Upgraded Text.h

--- src/Text.cpp
+++ src/Text.cpp
@@ -3,7 +3,8 @@
 
 Text::Text()
 {
-
+	rect.x = 0;
+	rect.y = 0;
 }
 
 Text::~Text()
@@ -14,6 +15,7 @@ Text::~Text()
 
 void Text::LoadFont(const char* font_name, Uint8 _size)
 {
+	fontName = font_name;
 	size = _size;
 	font = TTF_OpenFont(font_name, size);
 
@@ -23,21 +25,80 @@ void Text::LoadFont(const char* font_name, Uint8 _size)
 	}
 }
 
-void Text::LoadText(const char* text_content)
+void Text::LoadTextContent(const char* _textContent)
 {
-	SDL_Color auxColor = { 255,255,255,255 };
-	SDL_Surface* auxSurface = TTF_RenderText_Solid(font, text_content, auxColor);
-	text = SDL_CreateTextureFromSurface(sRenderer->getRenderer(), auxSurface);
+	textContent = _textContent;
 
-	SDL_QueryTexture(text, NULL, NULL, &texW, &texH);
+	SDL_Color auxColor = { 255,255,255,255 };
+	SDL_Surface* auxSurface = TTF_RenderText_Solid(font, _textContent, auxColor);
+	texture = SDL_CreateTextureFromSurface(sRenderer->getRenderer(), auxSurface);
+
+	SDL_QueryTexture(texture, NULL, NULL, &texW, &texH);
+
+	rect.w = texW;
+	rect.h = texH;
+}
+
+void Text::AddaptFontSizeToWidth(Uint16 _buttonWidth, SDL_Rect _rect, Padding _padding)
+{
+	int aTexW = 0, aTexH = 0;
+
+	bool loopeble = true;
+	while (loopeble)
+	{
+		SDL_QueryTexture(texture, NULL, NULL, &aTexW, &aTexH);
+		
+		if (aTexW >= _buttonWidth)
+		{
+			size--;
+			ReloadFont();
+			ReloadTextContent();
+		}
+		else
+		{
+			loopeble = false;
+			texW = aTexW;
+			texH = aTexH;
+			
+			rect = _rect;
+			rect.x += (rect.w - (rect.w / 2)) - texW / 2;
+			rect.y += (rect.h - (rect.h / 2) - texH / 2);
+			rect.w = texW;
+			rect.h = texH;
+		}
+	}
 }
 
 void Text::RenderFont()
 {
-	sRenderer->drawText(text, SDL_Rect{ 400, 0, texW, texH});
+	sRenderer->drawText(texture, rect);
 }
 
 void Text::RenderFont(SDL_Rect rect)
 {
-	sRenderer->drawText(text, rect);
+	sRenderer->drawText(texture, rect);
+}
+
+void Text::ReloadFont()
+{
+	if (font)
+	{
+		TTF_CloseFont(font);
+		font = NULL;
+	}
+
+	font = TTF_OpenFont(fontName, size);
+
+	if (!font)
+	{
+		printf("TEXT: Failed to reload the font. ERROR: %s", TTF_GetError());
+	}
+}
+
+void Text::ReloadTextContent()
+{
+	SDL_Color auxColor = { 255,255,255,255 };
+	SDL_Surface* auxSurface = TTF_RenderText_Solid(font, textContent, auxColor);
+	texture = SDL_CreateTextureFromSurface(sRenderer->getRenderer(), auxSurface);
+
 }
--- src/Text.h
+++ src/Text.h
@@ -4,6 +4,8 @@
 #include <SDL_ttf.h>
 #include <istream>
 
+#include "Structures.h"
+
 class Text
 {
 public:
@@ -11,14 +13,23 @@ public:
 	~Text();
 
 	void LoadFont(const char* font_name, Uint8 size);
-//	void LoadFont(const char* font_name, Uint16 width);
-	void LoadText(const char* text_content);
+	void LoadTextContent(const char* text_content);
+
+	void AddaptFontSizeToWidth(Uint16 _buttonWidth, SDL_Rect _rect, Padding _padding);
+
 	void RenderFont();
 	void RenderFont(SDL_Rect rect);
 
+	const char* textContent;
 private:
+	void ReloadFont();
+	void ReloadTextContent();
+
+	const char* fontName;
+
 	TTF_Font* font;
-	SDL_Texture* text;
+	SDL_Rect rect;
+	SDL_Texture* texture;
 	Uint8 size;
 	int texW, texH;
 };
